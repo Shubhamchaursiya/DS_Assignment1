9a8efac13b4955ec0216b8865005ca76804bc33a
Shubham <shubham.chaursiya@hotwaxsystems.com>
2021-12-21 22:05:07 +0530

    Sorting the element by using Merge Sort

---
+++ src/com/hotwax/sorting/MergeSort.java
@@ -0,0 +1,84 @@
+package com.hotwax.sorting;
+
+public class MergeSort {
+    static void merge(int a[], int beg, int mid, int end)
+    {
+        int i, j, k;
+        int n1 = mid - beg + 1;
+        int n2 = end - mid;
+
+        /* temporary Arrays */
+        int LeftArray[] = new int[n1];
+        int RightArray[] = new int[n2];
+
+        /* copy data to temp arrays */
+        for (i = 0; i < n1; i++)
+            LeftArray[i] = a[beg + i];
+        for (j = 0; j < n2; j++)
+            RightArray[j] = a[mid + 1 + j];
+
+        i = 0; /* initial index of first sub-array */
+        j = 0; /* initial index of second sub-array */
+        k = beg;  /* initial index of merged sub-array */
+
+        while (i < n1 && j < n2)
+        {
+            if(LeftArray[i] <= RightArray[j])
+            {
+                a[k] = LeftArray[i];
+                i++;
+            }
+            else
+            {
+                a[k] = RightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<n1)
+        {
+            a[k] = LeftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j<n2)
+        {
+            a[k] = RightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void mergeSort(int a[], int beg, int end)
+    {
+        if (beg < end)
+        {
+            int mid = (beg + end) / 2;
+            mergeSort(a, beg, mid);
+            mergeSort(a, mid + 1, end);
+            merge(a, beg, mid, end);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        // write your code here
+        int[] array = {10, 3, 4, 2, 11, 45, 76, 87, 2};
+        int n = array.length;
+        System.out.println("Before Sorting list:");
+        printArray(array);
+
+        mergeSort(array,0,n-1);
+        System.out.println("After Sorting list");
+        printArray(array);
+
+    }
+}
